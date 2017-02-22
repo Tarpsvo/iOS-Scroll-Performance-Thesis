836004e6d1c677863055e08207048919e7af7696
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2017-02-22 11:13:26 +0200

    Make image optimizer rounded corners optional and cached separately

--- iOS Scroll Test/iOS Scroll Test/Utils/ImageOptimizer.swift
+++ iOS Scroll Test/iOS Scroll Test/Utils/ImageOptimizer.swift
@@ -2,15 +2,16 @@ import UIKit
 import PINRemoteImage
 
 class ImageOptimizer {
-    static func processImage(image: UIImage, size: CGFloat) -> UIImage {
-        let imageRect = CGRect(x: 0, y: 0, width: size, height: size)
+    static func processImage(image: UIImage, size: CGFloat, makeRounded: Bool? = false) -> UIImage {
+        let scaledSize = (size * UIScreen.main.scale)
+        let imageRect = CGRect(x: 0, y: 0, width: scaledSize, height: scaledSize)
 
         UIGraphicsBeginImageContext(imageRect.size)
 
-        let sizeMultiplier: CGFloat = (size / image.size.width)
+        let sizeMultiplier: CGFloat = (scaledSize / image.size.width)
 
         var drawRect = CGRect(x: 0, y: 0, width: image.size.width * sizeMultiplier, height: image.size.height * sizeMultiplier)
-        UIBezierPath(roundedRect: drawRect, cornerRadius: 15.0).addClip()
+        if (makeRounded == true) { UIBezierPath(roundedRect: drawRect, cornerRadius: 15.0).addClip() }
         if (drawRect.maxX > imageRect.maxX) { drawRect.origin.x -= (drawRect.maxX - imageRect.maxX) / 2 }
         if (drawRect.maxY > imageRect.maxY) { drawRect.origin.y -= (drawRect.maxY - imageRect.maxY) / 2 }
 
@@ -22,7 +23,7 @@ class ImageOptimizer {
         return processedImage!
     }
 
-    static func handleAndSetImage(imageView: FLAnimatedImageView, urlString: String, size: CGFloat) {
+    static func handleAndSetImage(imageView: FLAnimatedImageView, urlString: String, size: CGFloat, makeRounded: Bool = false) {
         imageView.pin_clearImages()
         let url = URL(string: urlString)
 
@@ -30,9 +31,9 @@ class ImageOptimizer {
         if (urlString.substring(from: urlString.index(urlString.endIndex, offsetBy: -3)) == "gif") {
             imageView.pin_setImage(from: url)
         } else {
-            imageView.pin_setImage(from: url, processorKey: "scaled_\(size)") { (result, unsafePointer) -> UIImage? in
+            imageView.pin_setImage(from: url, processorKey: "scaled_\(size)_\(makeRounded)") { (result, unsafePointer) -> UIImage? in
                 guard let image = result.image else { return nil }
-                return ImageOptimizer.processImage(image: image, size: size)
+                return ImageOptimizer.processImage(image: image, size: size, makeRounded: makeRounded)
             }
         }
     }
